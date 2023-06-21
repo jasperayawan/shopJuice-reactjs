2a8bf4214f3c6604fa9188d71ac9aefe381c7f7c
jasperayawan <ejayawan22@gmail.com>
2023-06-21 23:41:17 +0800

    update text

--- client/src/components/Juice.jsx
+++ client/src/components/Juice.jsx
@@ -20,7 +20,7 @@ export default function Juice(){
         content: `Lorem ipsum dolor sit amet, 
                   consectetur adipiscing elit, 
                   sed do eiusmod tempor incididunt 
-                  ut labore et dolore magna aliqua. `
+                  ut labore et dolore magna. `
       }
     ];
 
